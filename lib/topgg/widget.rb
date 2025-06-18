e4acb299d71cf52fc2da82d2a30b945e8b3faf01
null8626 <vierofernando9@gmail.com>
2025-06-18 20:46:09 +0700

    feat: add small widgets

--- lib/topgg/widget.rb
+++ lib/topgg/widget.rb
@@ -5,11 +5,47 @@ module Dbl
 
     class << self
       attr_accessor :base_url
+
+      TYPES = [:discord_bot, :discord_server]
   
       # Generates a large widget URL.
+      # @param ty [Symbol] The widget type.
       # @param id [String] The ID.
-      def large(id)
-        "#{@base_url}/widgets/large/#{id}"
+      def large(ty, id)
+        raise ArgumentError, "Invalid widget type" unless TYPES.include?(ty)
+        type = ty.to_s.gsub('_', '/')
+
+        "#{@base_url}/widgets/large/#{type}/#{id}"
+      end
+
+      # Generates a small widget URL for displaying votes.
+      # @param ty [Symbol] The widget type.
+      # @param id [String] The ID.
+      def votes(ty, id)
+        raise ArgumentError, "Invalid widget type" unless TYPES.include?(ty)
+        type = ty.to_s.gsub('_', '/')
+
+        "#{@base_url}/widgets/small/votes/#{type}/#{id}"
+      end
+
+      # Generates a small widget URL for displaying an entity's owner.
+      # @param ty [Symbol] The widget type.
+      # @param id [String] The ID.
+      def owner(ty, id)
+        raise ArgumentError, "Invalid widget type" unless TYPES.include?(ty)
+        type = ty.to_s.gsub('_', '/')
+
+        "#{@base_url}/widgets/small/owner/#{type}/#{id}"
+      end
+
+      # Generates a small widget URL for displaying social stats.
+      # @param ty [Symbol] The widget type.
+      # @param id [String] The ID.
+      def social(ty, id)
+        raise ArgumentError, "Invalid widget type" unless TYPES.include?(ty)
+        type = ty.to_s.gsub('_', '/')
+        
+        "#{@base_url}/widgets/small/social/#{type}/#{id}"
       end
     end
   end
