7185abbbffc8e651fd6de0eee949bef7be81cb8e
null8626 <vierofernando9@gmail.com>
2025-10-10 00:30:01 +0700

    revert: revert breaking changes (part 1)

---
+++ lib/topgg/stats.rb
@@ -0,0 +1,34 @@
+module Dbl
+  # The Stats class spreads the JSON response into different methods
+  class Stats
+    # Initializes the Stats class
+    # @param obj [Object] Response Hash
+    def initialize(obj)
+      @obj = obj
+    end
+
+    # Returns raw Hash of the response
+    attr_reader :obj
+
+    alias raw obj 
+
+    alias data obj
+    # The bot's server count
+    # @return [Integer]
+    def server_count
+      @obj['server_count']
+    end
+
+    # The bot's server count in each shard
+    # @return [Integer]
+    def shards
+      @obj['shards']
+    end
+
+    # The bot's shard count
+    # @return [Integer]
+    def shard_count
+      @obj['shard_count']
+    end
+  end
+end
--- lib/topgg/user.rb
+++ lib/topgg/user.rb
@@ -35,5 +35,46 @@ module Dbl
     def avatar
       @obj["avatar"]
     end
+
+    # The default avatar of the user
+    # @return [String]
+    def defAvatar
+      ""
+    end
+
+    # Whether the user is a moderator.
+    # @return [Boolean]
+    def mod
+      false
+    end
+
+    # Whether the user is a Top.gg supporter.
+    # @return [Boolean]
+    def supporter
+      false
+    end
+
+    # Whether the user is a certified developer.
+    # @return [Boolean]
+    def certifiedDev
+      false
+    end
+
+    # The user's socials
+    # @return [Object]
+    def social
+      nil
+    end
+
+    # Whether the user is an administrator.
+    # @return [Boolean]
+    def admin
+      false
+    end
+
+    # Whether the user is a web moderator.
+    # @return [Boolean]
+    def webMod
+      false
   end
 end
